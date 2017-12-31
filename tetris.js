00432c145868271830bab615c97c5179695daf46
NS <newstyleage@gmail.com>
2017-12-31 17:46:59 +0100

    colors

--- tetris.js
+++ tetris.js
@@ -43,38 +43,38 @@ function createPiece(type) {
         ]
     } else if (type === "O") {
         return [
-            [1, 1],
-            [1, 1],
+            [2, 2],
+            [2, 2],
         ]
     } else if (type === "L") {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 1],
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
         ]
     } else if (type === "J") {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [1, 1, 0],
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
         ]
     } else if (type === "I") {
         return [
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
         ]
     } else if (type === "S") {
         return [
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
             [0, 0, 0],
         ]
     } else if (type === "Z") {
         return [
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
             [0, 0, 0],
         ]
     }
@@ -94,7 +94,7 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                ctx.fillStyle = "red";
+                ctx.fillStyle = colors[value];
                 ctx.fillRect(x + offset.x,
                     y + offset.y,
                     1, 1)
@@ -190,6 +190,16 @@ function update(time = 0) {
     draw();
     requestAnimationFrame(update);
 }
+const colors = [
+    null,
+    'red',
+    'blue',
+    'violet',
+    'green',
+    'purple',
+    'orange',
+    'pink',
+];
 
 const arena = createMatrix(12, 20);
 
